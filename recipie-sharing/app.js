cd699d37505fa42fb1485a3008b35b6d94435890
Ayush Baranwal <abaran803@gmail.com>
2025-08-30 23:23:09 +0530

    Swagger Implemented

--- recipie-sharing/app.js
+++ recipie-sharing/app.js
@@ -2,6 +2,8 @@ const cors = require("cors");
 const express = require("express");
 const authRoutes = require("./modules/auth/routes");
 const ingredientRoutes = require("./modules/ingredient/routes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 require("./model/index");
 
 const app = express();
@@ -12,5 +14,6 @@ app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/ingredients", ingredientRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
