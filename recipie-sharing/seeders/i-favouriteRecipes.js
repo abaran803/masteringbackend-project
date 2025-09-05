c0972f9a04affd528dcd84ec15d51003aa86bc37
Ayush Baranwal <abaran803@gmail.com>
2025-09-05 18:41:36 +0530

    Seeder file completely added

---
+++ recipie-sharing/seeders/i-favouriteRecipes.js
@@ -0,0 +1,17 @@
+"use strict";
+const fs = require("fs");
+const path = require("path");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const data = fs.readFileSync(
+      path.join(__dirname, "..", "seed-data", "favouriteRecipes.json")
+    );
+    const stringified = data.toString();
+    const parsed = JSON.parse(stringified);
+    await queryInterface.bulkInsert("favouriteRecipes", parsed);
+  },
+
+  async down(queryInterface, Sequelize) {},
+};
