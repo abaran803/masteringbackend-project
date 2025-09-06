a98834f688a1fb58058f498a5536c4e617ba8b13
Ayush Baranwal <abaran803@gmail.com>
2025-09-07 01:29:00 +0530

    Force ssl cert added for cloud db connection

--- recipie-sharing/models/index.js
+++ recipie-sharing/models/index.js
@@ -22,6 +22,12 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
     logging: (log) => logStream.write(log + "\n"),
   }
 );
