2dc3ef6b2c38de385f59c784aac51cd0f03075fa
Ayush Baranwal <abaran803@gmail.com>
2025-09-07 00:12:12 +0530

    Netlify deployment changes

--- recipie-sharing/server.js
+++ recipie-sharing/server.js
@@ -10,10 +10,6 @@ const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
-});
-
 module.exports = server;
 
 if (require.main === module) {
